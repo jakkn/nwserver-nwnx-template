b0d4a40b86549ef90904924ab5da2d1eea341500
Jakob Knutsen <jakobknutsen@gmail.com>
2024-04-27 16:33:34 +0200

    Support init of Anvil plugins

--- init.sh
+++ init.sh
@@ -26,12 +26,40 @@ __setup_nwnx() {
   _write_to_env_file IMAGE_TAG "ghcr.io/nwnxee/unified:$_nwnx_version" .env
 }
 
+__setup_anvil() {
+  # enable plugin nwnx_dotnet
+  _write_to_env_file NWNX_DOTNET_SKIP n "$_toplevel/config/nwserver.env"
+
+  # Ask which anvil version we want to use
+  _versions=$(__get_list_of_release_tags_for_repo nwn-dotnet/anvil)
+  _anvil_version=$(printf "%s\n" "$_versions" | _pipe_select)
+
+  _write_to_env_file IMAGE_TAG "ghcr.io/nwn-dotnet/anvil:$_anvil_version" .env
+}
+
+__setup_anvil_plugin() {
+  # Ask if we want to create an Anvil plugin
+  read -r -p "Do you want to create an Anvil plugin? [Y/n] " _response
+  if [[ ! "$_response" =~ ^([nN][oO]|[nN])$ ]]
+  then
+    # Test if we have dotnet installed
+    command -v dotnet >/dev/null 2>&1 || { _warn "dotnet is not installed. We need it to create a plugin for Anvil.\nIf you want help, install dotnet and run this script again."; return 0; }
+    dotnet new install NWN.Templates
+    read -r -p "Name the plugin folder [default: NWN.MyPlugin] " _plugin_folder
+    read -r -p "Name the plugin [default: MyPlugin] " _plugin_name
+    _plugin_folder=${_plugin_folder:-NWN.MyPlugin}
+    _plugin_name=${_plugin_name:-MyPlugin}
+    dotnet new anvilplugin --output "$_plugin_folder" --name "$_plugin_name"
+  fi
+}
+
 
 ## Main ##
 
 _choice=$(echo -e "nwnx\nAnvil" | _pipe_select)
 case $_choice in
   nwnx) __setup_nwnx;;
+  Anvil) __setup_anvil; __setup_anvil_plugin;;
   *) _die "Invalid option";;
 esac
 
