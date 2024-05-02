998103f8d99116ca21e4c874c018d1dafd3684d5
Jakob Knutsen <jakobknutsen@gmail.com>
2024-05-02 16:52:09 +0200

    Reorder run-commands

--- run-commands.sh
+++ run-commands.sh
@@ -7,15 +7,15 @@ source "$__scripts_path/nwnx_utils.sh"
 
 PS3="Use this menu to run commands. Choose an option: "
 
-options=("docker down & up" "nss watch" "extract module" "nss all" "pack module" "nwserver restart" "tail server logs" "download all plugin nss")
+options=("docker down & up" "nss watch" "nss all" "pack module" "unpack module" "nwserver restart" "tail server logs" "download all plugin nss")
 while true; do
   select option in "${options[@]}" Quit; do
     case $REPLY in
       1) docker compose -f docker-compose.yaml -f docker-compose.dev.yaml down && docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up -d; break;;
-      2) ./nwn-build.sh compile -w; break;;
-      3) nasher unpack; break;;
-      4) ./nwn-build.sh compile; break;;
-      5) nasher pack; break;;
+      2) ./nwn-build.sh compile --watch; break;;
+      3) nasher compile; break;;
+      4) nasher pack; break;;
+      5) nasher unpack; break;;
       6) docker compose restart nwserver; break;;
       7) docker compose logs -f nwserver; break;;
       8) _update_nwnx_nss; break;;
